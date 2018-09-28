2e86da95b2733508bcd726823eb95de3d774b7cc
kf07 <furusu0715@gmail.com>
2018-09-28 10:11:08 +0900

    gulpfile整形 タスクをアロー関数に統一

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const csscomb = require('gulp-csscomb');
 const plumber = require('gulp-plumber');
 const imagemin = require('gulp-imagemin');
 const pngquant = require('imagemin-pngquant');
-const mozjpeg  = require('imagemin-mozjpeg');
+const mozjpeg = require('imagemin-mozjpeg');
 const htmlbeautify = require('gulp-html-beautify');
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
@@ -19,15 +19,13 @@ const uglify = require('gulp-uglify')
 const cleanCSS = require('gulp-clean-css');
 const rename = require("gulp-rename");
 
-// webpackの設定ファイルの読み込み
-const webpackConfig = require('./webpack.config');
 
 // beautifiy_option
 const beautify_options = {
-    'indent_with_tabs':true
+    'indent_with_tabs': true
 };
 
-gulp.task('html', function() {
+gulp.task('html', () => {
     return gulp.src('dist/**/*.html')
         .pipe(htmlbeautify(beautify_options))
         .pipe(gulp.dest('dist/'))
@@ -43,22 +41,22 @@ gulp.task('pug', () => {
         .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('imagemin',() => {
+gulp.task('imagemin', () => {
     return gulp.src('src/img/*')
         .pipe(plumber())
         .pipe(imagemin([
-            pngquant({ quality: '65-85', speed: 1 }),
-            mozjpeg({ quality: 85 }),
+            pngquant({quality: '65-85', speed: 1}),
+            mozjpeg({quality: 85}),
             imagemin.svgo(),
             imagemin.gifsicle()
         ]))
         .pipe(gulp.dest('./dist/assets/img/'))
 });
 
-gulp.task('sass', function () {
+gulp.task('sass', () => {
     return gulp.src('src/sass/**/*.scss')
         .pipe(plumber({
-            errorHandler: function(err) {
+            errorHandler: function (err) {
                 console.log(err.messageFormatted);
                 this.emit('end');
             }
@@ -72,27 +70,26 @@ gulp.task('sass', function () {
         .pipe(csscomb())
         .pipe(postcss([
             autoprefixer({
-                // ☆IEは11以上、Androidは4.4以上
+                // IEは11以上、Androidは4.4以上
                 // その他は最新2バージョンで必要なベンダープレフィックスを付与する設定
                 browsers: ["last 2 versions", "ie >= 11", "Android >= 4"],
                 cascade: false
             })
         ]))
-        // cssフォルダー以下に保存
         .pipe(gulp.dest('./dist/assets/css'))
 });
 
 
 //babel
-gulp.task('babel', function () { //このタスクにbabelって名前をつける
-    gulp.src('src/es6/script.js') //変換したいES2015で記述したファイルを指定
+gulp.task('babel', () => {
+    gulp.src('src/es6/script.js')
         .pipe(babel())
-        .pipe(gulp.dest('./dist/assets/js')); //gulp.dest()でファイルの書き出し 今回はjsフォルダに
+        .pipe(gulp.dest('./dist/assets/js'));
 });
 
 
 //browser-sync
-gulp.task('browser-sync',()=> {
+gulp.task('browser-sync', () => {
     browserSync({
         server: {
             baseDir: "./dist"
@@ -108,7 +105,7 @@ gulp.task('minify-css', () => {
 });
 
 //js-min
-gulp.task("minify-js", ()=> {
+gulp.task("minify-js", () => {
     return gulp.src("dist/assets/js/**/*.js")
         .pipe(uglify())
         .pipe(gulp.dest("dist/assets/js/"));
@@ -117,22 +114,23 @@ gulp.task("minify-js", ()=> {
 //html-min
 gulp.task('minify', () => {
     return gulp.src('dist/**/*.html')
-        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('bs-reload', ()=> {
+
+gulp.task('bs-reload', () => {
     browserSync.reload();
 });
 
 
 // default
-gulp.task('default',['browser-sync'], ()=> {
-    gulp.watch('src/pug/**/*.pug',['pug']);
-    gulp.watch('src/**/*.scss',['sass']);
-    gulp.watch('src/**/*.js',['babel']);
-    gulp.watch('dist/**/*.html',['html']);
-    gulp.watch('dist/**/*.html',['bs-reload']);
-    gulp.watch('dist/**/*.js',['bs-reload']);
-    gulp.watch('dist/**/*.css',['bs-reload']);
+gulp.task('default', ['browser-sync'], () => {
+    gulp.watch('src/pug/**/*.pug', ['pug']);
+    gulp.watch('src/**/*.scss', ['sass']);
+    gulp.watch('src/**/*.js', ['babel']);
+    gulp.watch('dist/**/*.html', ['html']);
+    gulp.watch('dist/**/*.html', ['bs-reload']);
+    gulp.watch('dist/**/*.js', ['bs-reload']);
+    gulp.watch('dist/**/*.css', ['bs-reload']);
 });
