111f844f94125422909220e4bf4ab4a3aade8747
furus <furusu0715@gmail.com>
2018-10-07 02:00:01 +0900

    gulpfile整形

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-// 必要プラグインの読み込み (var gulp = ~ でも可)
 const gulp = require('gulp');
 const pug = require('gulp-pug');
 const sass = require('gulp-sass');
@@ -25,130 +24,130 @@ const prettierPlugin = require('gulp-prettier-plugin');
 
 // beautifiy_option
 const beautify_options = {
-    'indent_with_tabs': true
+  'indent_with_tabs': true
 };
 
 gulp.task('html', () => {
-    return gulp.src('dist/**/*.html')
-        .pipe(htmlbeautify(beautify_options))
-        .pipe(gulp.dest('dist/'))
+  return gulp.src('dist/**/*.html')
+    .pipe(htmlbeautify(beautify_options))
+    .pipe(gulp.dest('dist/'))
 });
 
 //pug
 gulp.task('pug', () => {
-    return gulp.src(['src/pug/**/*.pug', '!src/pug/**/_*.pug'])
-        .pipe(plumber())
-        .pipe(pug({
-            pretty: true
-        }))
-        .pipe(gulp.dest('./dist/'));
+  return gulp.src(['src/pug/**/*.pug', '!src/pug/**/_*.pug'])
+    .pipe(plumber())
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('imagemin', () => {
-    return gulp.src('src/img/*')
-        .pipe(plumber())
-        .pipe(imagemin([
-            pngquant({quality: '65-85', speed: 1}),
-            mozjpeg({quality: 85}),
-            imagemin.svgo(),
-            imagemin.gifsicle()
-        ]))
-        .pipe(gulp.dest('./dist/assets/img/'))
+  return gulp.src('src/img/*')
+    .pipe(plumber())
+    .pipe(imagemin([
+      pngquant({quality: '65-85', speed: 1}),
+      mozjpeg({quality: 85}),
+      imagemin.svgo(),
+      imagemin.gifsicle()
+    ]))
+    .pipe(gulp.dest('./dist/assets/img/'))
 });
 
 gulp.task('sass', () => {
-    return gulp.src('src/sass/**/*.scss')
-        .pipe(plumber({
-            errorHandler: function (err) {
-                console.log(err.messageFormatted);
-                this.emit('end');
-            }
-        }))
-        .pipe(sassGlob())
-        // Sassのコンパイルを実行
-        .pipe(sass({
-            outputStyle: 'expanded'
-        }))
-        .pipe(cmq())
-        .pipe(csscomb())
-        .pipe(postcss([
-            autoprefixer({
-                // IEは11以上、Androidは4.4以上
-                // その他は最新2バージョンで必要なベンダープレフィックスを付与する設定
-                browsers: ["last 2 versions", "ie >= 11", "Android >= 4"],
-                cascade: false,
-                grid: true
-            }),
-            // stylelint(),
-            // postcssReporter({clearMessages: true})
-        ]))
-        .pipe(gulp.dest('./dist/assets/css'))
+  return gulp.src('src/sass/**/*.scss')
+    .pipe(plumber({
+      errorHandler: function (err) {
+        console.log(err.messageFormatted);
+        this.emit('end');
+      }
+    }))
+    .pipe(sassGlob())
+    // Sassのコンパイルを実行
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }))
+    .pipe(cmq())
+    .pipe(csscomb())
+    .pipe(postcss([
+      autoprefixer({
+        // IEは11以上、Androidは4.4以上
+        // その他は最新2バージョンで必要なベンダープレフィックスを付与する設定
+        browsers: ["last 2 versions", "ie >= 11", "Android >= 4"],
+        cascade: false,
+        grid: true
+      }),
+      // stylelint(),
+      // postcssReporter({clearMessages: true})
+    ]))
+    .pipe(gulp.dest('./dist/assets/css'))
 });
 gulp.task('prettier', () => {
-    return gulp.src(['src/sass/**/*.scss', 'src/es6/*.js'])
-      .pipe(
-        prettierPlugin(
-          {
-            singleQuote: true,
-          },
-          {
-            filter: true
-          }
-        )
+  return gulp.src(['src/sass/**/*.scss', 'src/es6/*.js'])
+    .pipe(
+      prettierPlugin(
+        {
+          singleQuote: true,
+        },
+        {
+          filter: true
+        }
       )
-      .pipe(gulp.dest(file => file.base))
+    )
+    .pipe(gulp.dest(file => file.base))
 });
 
 
 //babel
 gulp.task('babel', () => {
-    return gulp.src('src/es6/**/*.js')
-        .pipe(babel())
-        .pipe(gulp.dest('./dist/assets/js'));
+  return gulp.src('src/es6/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('./dist/assets/js'));
 });
 
 
 //browser-sync
 gulp.task('browser-sync', () => {
-    browserSync({
-        server: {
-            baseDir: "./dist"
-        }
-    });
+  browserSync({
+    server: {
+      baseDir: "./dist"
+    }
+  });
 });
 
 //css-min
 gulp.task('minify-css', () => {
-    return gulp.src('dist/assets/css/**/*.css')
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest('dist/assets/css/'));
+  return gulp.src('dist/assets/css/**/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist/assets/css/'));
 });
 
 //js-min
 gulp.task("minify-js", () => {
-    return gulp.src("dist/assets/js/**/*.js")
-        .pipe(uglify())
-        .pipe(gulp.dest("dist/assets/js/"));
+  return gulp.src("dist/assets/js/**/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("dist/assets/js/"));
 });
 
 //html-min
 gulp.task('minify-html', () => {
-    return gulp.src('dist/**/*.html')
-        .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest('dist'));
+  return gulp.src('dist/**/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('bs-reload', () => {
-    browserSync.reload();
+  browserSync.reload();
 });
 
 
 // default
 gulp.task('default', ['browser-sync'], () => {
-    gulp.watch('src/pug/**/*.pug', ['pug']);
-    gulp.watch('src/**/*.scss', ['prettier','sass']);
-    gulp.watch('src/**/*.js', ['prettier','babel','bs-reload']);
-    gulp.watch('dist/**/*.html', ['html']);
-    gulp.watch('dist/**/*.html', ['bs-reload']);
-    gulp.watch('dist/**/*.css', ['bs-reload']);
+  gulp.watch('src/pug/**/*.pug', ['pug']);
+  gulp.watch('src/**/*.scss', ['prettier', 'sass']);
+  gulp.watch('src/**/*.js', ['prettier', 'babel', 'bs-reload']);
+  gulp.watch('dist/**/*.html', ['html']);
+  gulp.watch('dist/**/*.html', ['bs-reload']);
+  gulp.watch('dist/**/*.css', ['bs-reload']);
 });
