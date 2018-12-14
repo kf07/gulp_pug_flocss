4ade98ae716726de3fceec9c333382506b8077a7
kf07 <furusu0715@gmail.com>
2018-12-14 17:56:34 +0900

    重複回避途中

--- src/es6/scripts.js
+++ src/es6/scripts.js
@@ -1,3 +1,29 @@
 const app = new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    activeClass: false,
+    NumItem: ['item1','item2','item3','item4','item5','item6','item7','item8','item9'],
+    Num: [1,2,3,4,5,6,7,8,9],
+    isActive: true,
+    aaa: 'red'
+  },
+  methods: {
+    handleClick: function handleClick() {
+      const min = 1;
+      const max = 9;
+      const beforeNum =[].concat(this.Num);
+      this.Num = [];
+      this.NumItem = [];
+      for (let i = 0; i < 9; i++){
+        let random = Math.floor(Math.random() * (max + 1 - min) + min);
+        while(this.Num.indexOf(random) >= 0 && random !== beforeNum[i]){
+          random = Math.floor(Math.random() * (max + 1 - min) + min);
+        }
+        const randomItem = 'item' + random;
+        this.Num.push(random);
+        console.log(random,beforeNum[i]);
+        this.NumItem.push(randomItem);
+      }
+    }
+  }
 });
