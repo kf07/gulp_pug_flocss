129e0894d5d194b60fd62dbe68f4293fd328ebff
kango furusu <furusu0715@gmail.com>
2018-12-19 16:13:33 +0900

    記事一覧レイアウト

--- src/es6/scripts.js
+++ src/es6/scripts.js
@@ -1,22 +1,22 @@
 const app = new Vue({
   el: '#app',
   data: {
-    order:false,
+    order: false,
     list: [
-      {id:1, name:'01'},
-      {id:2, name:'02'},
-      {id:3, name:'03'},
-      {id:4, name:'04'},
-      {id:5, name:'05'},
-      {id:6, name:'06'},
-      {id:7, name:'07'},
-      {id:8, name:'08'},
-      {id:9, name:'09'}
+      { id: 1, name: '01' },
+      { id: 2, name: '02' },
+      { id: 3, name: '03' },
+      { id: 4, name: '04' },
+      { id: 5, name: '05' },
+      { id: 6, name: '06' },
+      { id: 7, name: '07' },
+      { id: 8, name: '08' },
+      { id: 9, name: '09' }
     ]
   },
   computed: {
-    sortedList: function(){
-      return _.shuffle(this.list,'id', this.order ? 'desc': 'asc')
+    sortedList: function() {
+      return _.shuffle(this.list, 'id', this.order ? 'desc' : 'asc');
     }
   }
 });
